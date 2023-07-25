cc209cc3064df7b9757307a9e6dcdb7a52e1bca6
enjoy61 <enjoy61@protonmail.com>
2023-07-25 22:05:04 +0800

    custom: add highlight.js languages for c, cpp, lisp, cmake, csharp, scss, lua and makefile

--- assets/js/highlight.js
+++ assets/js/highlight.js
@@ -19,6 +19,31 @@ hljs.registerLanguage('yaml', yaml);
 hljs.registerLanguage('md', markdown);
 hljs.registerLanguage('python', python);
 
+import c from 'highlight.js/lib/languages/c';
+hljs.registerLanguage('c', c);
+
+import cpp from 'highlight.js/lib/languages/cpp';
+hljs.registerLanguage('cpp', cpp);
+
+import lisp from 'highlight.js/lib/languages/lisp';
+hljs.registerLanguage('elisp', lisp);
+hljs.registerLanguage('lisp', lisp);
+
+import cmake from 'highlight.js/lib/languages/cmake';
+hljs.registerLanguage('cmake', cmake);
+
+import csharp from 'highlight.js/lib/languages/csharp';
+hljs.registerLanguage('csharp', csharp);
+
+import scss from 'highlight.js/lib/languages/scss';
+hljs.registerLanguage('scss', scss);
+
+import lua from 'highlight.js/lib/languages/lua';
+hljs.registerLanguage('lua', lua);
+
+import makefile from 'highlight.js/lib/languages/makefile';
+hljs.registerLanguage('makefile', makefile);
+
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelectorAll('pre code:not(.language-mermaid)').forEach((block) => {
     hljs.highlightElement(block);
